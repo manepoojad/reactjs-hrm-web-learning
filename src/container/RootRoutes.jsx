b400f67b2d84e15fcc0e2725ca5e19f7162aa59a
Pooja Mane <manepoojad@gmail.com>
2024-06-04 23:39:23 +0530

    auth routes

--- src/container/RootRoutes.jsx
+++ src/container/RootRoutes.jsx
@@ -1,14 +1,37 @@
+import Cookies from "js-cookie";
 import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import Login from "../pages/Login";
 import PostAuthRoutes from "./PostAuthRoutes";
 
 const RootRoutes = () => {
+  const isAuthStatus = (() => {
+    let userToken = Cookies.get("token") || "";
+    if (userToken) {
+      return true;
+    } else {
+      return false;
+    }
+  })();
+
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/login" element={<Login />} />
-        <Route path="/*" element={<PostAuthRoutes />} />
+        <Route
+          path="/login"
+          element={isAuthStatus ? <Navigate replace to="/" /> : <Login />}
+        />
+        <Route
+          path="/*"
+          element={
+            isAuthStatus ? (
+              <PostAuthRoutes />
+            ) : (
+              <Navigate replace to="/login" />
+            )
+          }
+        />
       </Routes>
     </BrowserRouter>
   );
