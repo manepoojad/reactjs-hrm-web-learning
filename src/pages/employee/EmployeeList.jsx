0e8f0a53012bcb64e7d7faed0e8af27e5520e964
Pooja Mane <manepoojad@gmail.com>
2024-06-06 07:46:21 +0530

    search issue resolved

--- src/pages/employee/EmployeeList.jsx
+++ src/pages/employee/EmployeeList.jsx
@@ -3,45 +3,71 @@ import { useNavigate } from "react-router-dom";
 import { API_ROUTES_PATH } from "../../helper/Constants";
 import fetchInterceptor from "../../helper/fetchInterceptor";
 
-
 const EmployeeList = () => {
   const navigate = useNavigate();
   const [employeeList, setEmployeeList] = useState([]);
+  const [filteredEmployeeList, setFilteredEmployeeList] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 5;
+
 
   useEffect(() => {
     getEmployeeList();
-  }, []);
+  }, [currentPage]);
 
   const getEmployeeList = async () => {
     try {
-      // const jwtToken = Cookies.get("jwtToken");
-      // const response = await fetch(API_ROUTES_PATH.GET_EMPLOYEE_LIST, {
-      //   method: "GET",
-      //   headers: {
-      //     "Content-Type": "application/json",
-      //     Authorization: `${jwtToken}`,
-      //   },
-      // });
-      // if (!response.ok) {
-      //   throw new Error("Response not ok. ");
-      // }
-      // const responseData = await response.json();
-      const responseData = await fetchInterceptor(API_ROUTES_PATH.GET_EMPLOYEE_LIST, {
-        method: "GET"
-      })
-      setEmployeeList(responseData);
+      const responseData = await fetchInterceptor(
+        API_ROUTES_PATH.GET_EMPLOYEE_LIST,
+        {
+          method: "GET",
+        }
+      );
+
+      setEmployeeList(responseData?.employeeList);
+      setFilteredEmployeeList(responseData?.employeeList)
     } catch (error) {}
   };
 
   const addEmployee = () => {
     navigate("/employee/create");
   };
+
   const handleDeleteEmployee = (id) => {
     console.log(id);
   };
+
+  const handleSearchEmployee = (e) => {
+    const { value } = e.target;
+
+    const filteredList =
+      employeeList &&
+      employeeList.filter((item, index) => {
+        const fullName = `${item.firstName} ${item.lastName}`;
+
+        return (
+          item.firstName.toLowerCase().includes(value.toLowerCase()) ||
+          item.lastName.toLowerCase().includes(value.toLowerCase()) ||
+          fullName.toLowerCase().includes(value.toLowerCase()) ||
+          item.email.toLowerCase().includes(value.toLowerCase())
+        );
+      });
+
+    setFilteredEmployeeList(filteredList);
+  };
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const currentItems =
+    filteredEmployeeList?.length > 0
+      ? filteredEmployeeList?.slice(indexOfFirstItem, indexOfLastItem)
+      : filteredEmployeeList &&
+        filteredEmployeeList?.slice(indexOfFirstItem, indexOfLastItem);
+
   return (
     <>
       <div className="d-flex justify-content-end">
+        <input type="text" onChange={(e) => handleSearchEmployee(e)} />
         <button
           type="button"
           className="me-3  btn btn-dark mb-2 ms-1"
@@ -62,11 +88,11 @@ const EmployeeList = () => {
           </tr>
         </thead>
         <tbody>
-          {employeeList?.employeeList &&
-            employeeList?.employeeList.map((employee, index) => {
+          {currentItems && currentItems?.length > 0 ? (
+            currentItems.map((employee, index) => {
               return (
                 <tr key={index}>
-                  <td>{index + 1}</td>
+                  <td>{index + 1 + indexOfFirstItem}</td>
                   <td>{employee?.firstName}</td>
                   <td>{employee?.lastName}</td>
                   <td>{employee?.email}</td>
@@ -74,7 +100,6 @@ const EmployeeList = () => {
                   <td>
                     <button
                       className="btn btn-outline-success btn-sm mx-2"
-                      // title="Edit Employee Details"
                       onClick={() => {
                         navigate(`/employee/${employee.id}`);
                       }}
@@ -104,10 +129,41 @@ const EmployeeList = () => {
                   </td>
                 </tr>
               );
-            })}
+            })
+          ) : (
+            <tr>
+              <td colSpan="6">No data available</td>
+            </tr>
+          )}
         </tbody>
       </table>
+      <div>
+        <button
+          onClick={() => setCurrentPage(currentPage - 1)}
+          disabled={currentPage === 1}
+        >
+          Previous
+        </button>
+        <span
+          style={{
+            margin: "8px",
+            padding: 8,
+            backgroundColor: "GrayText",
+            borderRadius: 30,
+          }}
+        >
+          {" "}
+          {currentPage}
+        </span>
+        <button
+          onClick={() => setCurrentPage(currentPage + 1)}
+          disabled={currentItems?.length < itemsPerPage}
+        >
+          Next
+        </button>
+      </div>
     </>
   );
 };
+
 export default EmployeeList;
